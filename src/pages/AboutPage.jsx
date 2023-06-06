9aa525d63c8b6a4d4c68b70ea7c06a20e45c1e76
dorian-rose <dorianrkelly@gmail.com>
2023-06-06 11:41:21 +0200

    nav link functionality added

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
 
 export const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div>
+      <h1 className="font-light tracking-widest mt-4 mx-8 text-2xl sm:text-4xl text-center text-primary">
+        About me
+      </h1>
+      <p className="text-center font-thin mt-6">
+        Coming soon, please check back later, or visit my
+        <a
+          className="mx-1 text-primary hover:underline"
+          href="https://www.linkedin.com/in/dorian-kelly/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        for more information.
+      </p>
+    </div>
+  );
+};
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
 export const ProjectPage = () => {
-  return <div>ProjectPage</div>;
+  return (
+    <div>
+      <h1 className="font-light tracking-widest mt-4 mx-8 text-2xl sm:text-4xl text-center text-primary">
+        Projects
+      </h1>
+      <p className="text-center font-thin mt-6">
+        Full portfolio of projects being updated. Please check back again soon,
+        and in the meantime feel free to explore my
+        <a
+          className="ms-1 text-primary hover:underline"
+          href="https://github.com/dorian-rose"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub!
+        </a>
+      </p>
+    </div>
+  );
 };
